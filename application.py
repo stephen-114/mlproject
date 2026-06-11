63f4a2f4a9960e2a709a8d9baf801b9d211367f5
KANDUKURI STEPHEN PAUL <stephenkandukuri114@gmail.com>
2026-06-11 13:08:12 +0530

    Change default page to home.html

--- application.py
+++ application.py
@@ -6,14 +6,13 @@ from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
-
 app=application
 
 ## Route for a home page
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('home.html')
 
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
